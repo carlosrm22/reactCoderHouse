d55cd09a2ee7eb628e87f0359f7f148be2c0eb3d
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2023-04-26 18:20:20 -0600

    se corrige .gitignore

--- pre-entrega1-romero/src/components/NavBar.js
+++ pre-entrega1-romero/src/components/NavBar.js
@@ -2,3 +2,20 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">Mi tienda</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#">Categoría 1</Nav.Link>
+                    <Nav.Link href="#">Categoría 2</Nav.Link>
+                    <Nav.Link href="#">Categoría 3</Nav.Link>
+                </Nav>
+                <CartWidget />
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
