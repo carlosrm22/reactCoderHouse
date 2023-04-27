29d5f6d716348fb67ae371db98337325317e5948
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2023-04-26 18:26:33 -0600

    se crea props para container

--- pre-entrega1-romero/src/components/ItemListContainer.js
+++ pre-entrega1-romero/src/components/ItemListContainer.js
@@ -2,3 +2,13 @@ import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+function ItemListContainer(props) {
+    return (
+        <Container className="py-3">
+            <h2>{props.greeting}</h2>
+        </Container>
+    );
+}
+
+export default ItemListContainer;
