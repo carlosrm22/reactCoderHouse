35723fe6e199fcc4031d07c4a0207bb3429ad9d6
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2023-04-26 18:25:15 -0600

    se importa React en ItemListContainer.js

--- pre-entrega1-romero/src/components/ItemListContainer.js
+++ pre-entrega1-romero/src/components/ItemListContainer.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
